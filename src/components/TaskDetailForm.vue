ebc0e26c07e19fd5e8b0c2adb213b2aa0d8abc3a
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-03-11 18:05:36 +0800

    Just dont use wechat browser

--- src/components/TaskDetailForm.vue
+++ src/components/TaskDetailForm.vue
@@ -3,7 +3,7 @@
     <textarea
       type="text"
       :rows="taskDescription.split('\n').length + 1"
-      class="max-w-sm p-5 rounded-xl shadow-lg resize-none bg-white border-none focus:outline-primary dark:bg-slate-600 dark:drop-shadow-lg"
+      class="w-full p-5 rounded-xl shadow-lg resize-none bg-white border-none focus:outline-primary dark:bg-slate-600 dark:drop-shadow-lg"
       placeholder="To do this, I need to..."
       v-model.lazy.trim="taskDescription"
     />
--- src/views/TaskDetailView.vue
+++ src/views/TaskDetailView.vue
@@ -10,11 +10,11 @@
       <p>Oops, nothing here.</p>
     </div>
     <div v-else>
-      <div>
+      <div class="max-w-sm mx-auto">
         <h2 class="pb-5 text-xl">Task detail</h2>
         <RouterLink :to="{ name: 'home' }">
           <h3
-            class="text-lg max-w-sm py-3 my-5 mx-auto bg-gray-50 rounded-lg shadow-lg dark:bg-slate-700 dark:drop-shadow-lg"
+            class="w-full text-lg py-3 my-5 bg-gray-50 rounded-lg shadow-lg dark:bg-slate-700 dark:drop-shadow-lg"
           >
             {{ task.title }}
           </h3>

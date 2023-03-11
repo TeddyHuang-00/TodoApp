e50130dca2d47646fd137b316b66331e75d19722
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-03-11 15:05:12 +0800

    Add more padding to form for a nicer view

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form
     @submit.prevent="handleSubmit"
-    class="max-w-lg pt-5 my-0 mx-auto grid grid-cols-[3fr_1fr] gap-3"
+    class="max-w-lg pt-8 pb-3 my-0 mx-auto grid grid-cols-[3fr_1fr] gap-3"
   >
     <input
       class="border-none p-3 rounded-lg bg-gray-50 text-gray-600 shadow-lg focus:shadow-md transition-shadow focus:ring-primary focus:border-primary dark:bg-slate-800 dark:text-slate-200 dark:focus:bg-slate-700"
